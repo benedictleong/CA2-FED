ab9d702c2160e1020e0cc6a870dc905d8376dae3
benedictleong <bleon903@gmail.com>
2023-07-31 13:34:49 +0800

    alert

--- login.js
+++ login.js
@@ -1,13 +1,14 @@
-function login() {
+function login(event) {
+	event.preventDefault();
 	var username = document.getElementById("username").value;
 	var password = document.getElementById("password").value;
 
 	// Perform validation
 	if (username == "" || password == "") {
-		alert("Please enter your username and password");
+		document.getElementById("login-message").innerHTML = '<div class="alert alert-warning" role="alert">Please enter your username and/or password!</div>';
 	} else if (username != "admin" || password != "password") {
-		alert("Invalid username or password");
+		document.getElementById("login-message").innerHTML = '<div class="alert alert-danger" role="alert">Invalid username or password!</div>';
 	} else {
-		alert("Login successful");
+		document.getElementById("login-message").innerHTML = '<div class="alert alert-success" role="alert">Login Succesful!</div>';
 	}
 }
